cb1a67c2044186f170016f854efd119856a6326e
janu8889 <jansumeni@gmx.de>
2026-05-27 18:25:05 +0300

    numar tel

--- src/app/privacy/privacyData.js
+++ src/app/privacy/privacyData.js
@@ -106,7 +106,7 @@ const privacySections = [
         "items": [
           {
             "strong": "Phone calls",
-            "text": "from Kash Automotive Group LLC at 218-505-0788 regarding your inquiry and related vehicle information"
+            "text": "from Kash Automotive Group LLC at 000-000-0000 regarding your inquiry and related vehicle information"
           },
           {
             "strong": "SMS/Text messages",
