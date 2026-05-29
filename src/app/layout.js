0bae5089a41a2d604fa3c228353d4e00a1e0ad16
janu8889 <jansumeni@gmx.de>
2026-05-29 13:21:51 +0300

    u fb meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./homepage.css";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
+import Script from "next/script";
 
 export const metadata = {
   metadataBase: new URL("https://kashautomotive.com"),
@@ -49,12 +50,41 @@ export const metadata = {
   },
 };
 
-
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+
+        {/* Meta Pixel */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;
+            n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)
+            }(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '1638192400599872');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        {/* Noscript fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1638192400599872&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         <Header />
         {children}
         <Footer />
